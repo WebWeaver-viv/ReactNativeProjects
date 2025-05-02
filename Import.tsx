4f5c3c933b08810b61527ae8eaf6d1ad2c75ec18
Jitendra Gupta <jgjitendragupta62@gmail.com>
2025-05-02 11:37:49 +0530

    Update Import.tsx

--- Import.tsx
+++ Import.tsx
@@ -1,3 +1,5 @@
+//import React from 'react';
+//import {Text, View, SafeAreaView, StyleSheet, ScrollView} from 'react-native';
 import React from 'react';
 import {Text, View, SafeAreaView, StyleSheet, ScrollView} from 'react-native';
 
