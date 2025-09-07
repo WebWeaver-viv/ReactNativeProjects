35750cf90cacc130e2b92c60611ee3dae9d2895b
Jitendra Gupta <jgjitendragupta62@gmail.com>
2025-09-07 23:12:09 +0530

    Update Import.tsx

--- Import.tsx
+++ Import.tsx
@@ -41,3 +41,46 @@ const Styles = StyleSheet.create({
   },
 });
 export default App;
+//code of the 
+import React from 'react';
+import {Text, View, SafeAreaView, StyleSheet, ScrollView} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView style={Styles.container}>
+      <ScrollView>
+        <Text style={Styles.header}>WleCome To My App</Text>
+        <View style={Styles.box}>
+          <Text style={Styles.txt}>Hello World</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 0,
+    backgroundColor: 'white',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    marginTop: 50,
+    marginLeft: 90,
+  },
+  box: {
+    padding: 20,
+    margin: 5,
+  },
+  txt: {
+    paddingLeft: 20,
+    color: 'black',
+    fontSize: 24,
+    fontWeight: '600',
+    marginBottom: 20,
+    marginLeft: 90,
+  },
+});
+export default App;
