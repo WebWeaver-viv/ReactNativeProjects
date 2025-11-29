f15e42f94672329b8786529b7905bb6aeeaa3304
Jitendra Gupta <jgjitendragupta62@gmail.com>
2025-11-29 22:22:02 +0530

    Update App.js

--- App.js
+++ App.js
@@ -17,3 +17,22 @@ export default function App() {
     </NavigationContainer>
   );
 }
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import AppPro from './AppPro'; // Your Profile Screen
+import HomeDashboard from './HomeDashboard'; // New Home Screen
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+   //this is 
+     <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeDashboard} />
+        <Drawer.Screen name="Profile" component={AppPro} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
